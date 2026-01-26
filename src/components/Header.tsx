8b4c85e9b6ac72e3c648fb52e6d3bc50301b7f77
nastaran <nastarantalebi.1382@gmail.com>
2026-01-26 14:11:59 +0330

    add header and avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,15 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Header = () => {
   return (
-    <header className="flex h-14 items-center border-b px-4">
+    <header className="flex h-12 justify-end items-center border-b px-4">
       <div className="flex items-center gap-3">
-        <Avatar className="h-8 w-8">
+        <span className="text-sm font-medium">Nastaran Talebi</span>
+        <Avatar className="h-9 w-9">
           <AvatarImage src="https://github.com/shadcn.png" alt="User" />
-          <AvatarFallback>U</AvatarFallback>
+          <AvatarFallback>N</AvatarFallback>
         </Avatar>
 
-        <span className="text-sm font-medium">User</span>
       </div>
     </header>
   )
