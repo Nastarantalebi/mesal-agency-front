cc24bdce0a320304a9841e2374428d138aa6a1b8
nastaran <nastarantalebi.1382@gmail.com>
2026-02-21 17:25:48 +0330

    change redirection

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
-import CustomButton from '@/components/form/CustomButton'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import CustomButton from "@/components/form/CustomButton";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: HomeComponent,
-})
+});
 
 function HomeComponent() {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-center p-20">
-      
-      <CustomButton size="xl"><Link to='/dashboard'> Login</Link></CustomButton>
+      <CustomButton onClick={() => navigate({ to: "/dashboard" })} size="xl">
+        Login
+      </CustomButton>
     </div>
-  )
+  );
 }
