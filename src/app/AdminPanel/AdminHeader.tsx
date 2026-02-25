b9f9921597c070c56880141787c97fbcae72848a
nastaran <nastarantalebi.1382@gmail.com>
2026-02-25 11:54:57 +0330

    refactoringheader and add light primary colors to css file

--- src/app/AdminPanel/AdminHeader.tsx
+++ src/app/AdminPanel/AdminHeader.tsx
@@ -1,19 +1,55 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useNavigate } from "@tanstack/react-router";
+import { Bell, LogOut, User } from "lucide-react";
 
 const Header = () => {
-
+  const navigate = useNavigate();
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 flex h-12 justify-between items-center border-b px-4 bg-primary-60">
-      <div className="flex items-center gap-5 mr-5 text-white">
+    <div className="fixed top-0 left-0 right-0 h-20 backdrop-blur-sm z-[9] pointer-events-none">
+      <header className="fixed mx-5 my-2 rounded-xl top-0 left-0 right-0 z-10 flex h-14 justify-between items-center border px-4 bg-primary-10">
+        <div className="flex items-center gap-5 mr-5 text-primary">
           <span>آژانس</span>
-      </div>
-      <div className="flex items-center gap-3">
-        <span className="text-sm font-medium text-background">نسترن طالبی</span>
-        <Avatar className="h-9 w-9">
-          <AvatarFallback>N</AvatarFallback>
-        </Avatar>
-      </div>
-    </header>
+        </div>
+        <div className="flex items-center justify-center cursor-pointer gap-4 mx-2">
+          {/* <span className="text-sm font-medium text-background">نسترن طالبی</span> */}
+          <Bell className="text-primary w-4.5 h-4.5" />
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="h-9 w-9 cursor-pointer">
+                <AvatarFallback>
+                  <User className="text-primary" />
+                </AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start" className="items-end">
+              <div dir="rtl">
+                <DropdownMenuItem className="cursor-pointer gap-2 focus:bg-primary/10">
+                  <User className="w-4 h-4" />
+                  نسترن طالبی
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="cursor-pointer gap-2 focus:bg-primary/10"
+                  onClick={() => navigate({ to: "/" })}
+                >
+                  <LogOut className="w-4 h-4" />
+                  خروج
+                </DropdownMenuItem>
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+    </div>
   );
 };
 
--- src/test.tsx
+++ src/test.tsx
@@ -1,76 +1,244 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
-import { Locate } from "lucide-react";
-
-const FlyToLocation = ({ position }: { position: [number, number] }) => {
-  const map = useMap();
-  useEffect(() => {
-    map.flyTo(position, 15);
-  }, [position]);
-  return null;
-};
-
-const LocationPicker = ({ onPick }: { onPick: (pos: [number, number]) => void }) => {
-  useMapEvents({
-    click(e) {
-      onPick([e.latlng.lat, e.latlng.lng]);
-    },
-  });
-  return null;
-};
-
-// button inside the map
-const CurrentLocationButton = ({ onLocate }: { onLocate: (pos: [number, number]) => void }) => {
-  const handleClick = () => {
-    navigator.geolocation.getCurrentPosition(
-      (pos) => onLocate([pos.coords.latitude, pos.coords.longitude]),
-      () => alert("دسترسی به موقعیت مکانی رد شد")
-    );
+import clsx from "clsx";
+import { useState } from "react";
+import { Menu } from "@/components/Headless";
+import QuickSearch from "@/components/QuickSearch";
+import ActivitiesPanel from "@/components/ActivitiesPanel";
+import NotificationsPanel from "@/components/NotificationsPanel";
+import SwitchAccount from "@/components/SwitchAccount";
+import Lucide from "@/components/Lucide";
+import { useNavigate } from "react-router-dom";
+import { AlignJustify } from "lucide-react";
+import DynamicBreadcrumb from "./DynamicBreadcrumb";
+import { useLogout } from "@/features/auth/_services/useLogout";
+import Modal from "@/components/Headless/Dialog/Modal";
+import useMe from "@/features/auth/_services/useMe";
+import { useAuthStore } from "@/features/auth/store/authStore";
+function Topbar({
+  setActiveMobileMenu,
+  setCompactMenuOnHover,
+  toggleCompactMenu,
+}: {
+  setActiveMobileMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  setCompactMenuOnHover: React.Dispatch<React.SetStateAction<boolean>>;
+  toggleCompactMenu: () => void;
+}) {
+  const navigate = useNavigate();
+  const { data } = useMe();
+  const [quickSearch, setQuickSearch] = useState(false);
+  const [switchAccount, setSwitchAccount] = useState(false);
+  const [notificationsPanel, setNotificationsPanel] = useState(false);
+  const [activitiesPanel, setActivitiesPanel] = useState(false);
+  const { mutateAsync: logoutApi, isPending } = useLogout();
+  const requestFullscreen = () => {
+    const el = document.documentElement;
+    if (el.requestFullscreen) el.requestFullscreen();
   };
-
+  const [logout, setLogout] = useState<boolean>(false);
+  const userData = useAuthStore((s) => s.userData);
   return (
-    <div className="leaflet-bottom leaflet-right">
-      <div className="leaflet-control mb-2 mr-2">
+    <div className="fixed top-0 inset-x-0 z-10 h-[65px] box bg-slate-50 border-x-0 border-t-0 rounded-none flex shadow-none">
+      <div
+        className={clsx([
+          "xl:bg-slate-100 flex-none flex items-center z-10 px-1 h-full xl:w-[275px] overflow-hidden relative duration-300 group-[.side-menu--collapsed]:xl:w-[91px] group-[.side-menu--collapsed.side-menu--on-hover]:xl:w-[275px] group-[.side-menu--collapsed.side-menu--on-hover]:xl:shadow-[6px_0_12px_-4px_#0000001f]",
+          "before:content-[''] before:hidden before:xl:block before:absolute before:end-0 before:border-e before:border-slate-300/50 before:h-full",
+        ])}
+        onMouseOver={(event) => {
+          event.preventDefault();
+          setCompactMenuOnHover(true);
+        }}
+        onMouseLeave={(event) => {
+          event.preventDefault();
+          setCompactMenuOnHover(false);
+        }}>
+        <a
+          href=""
+          className="hidden xl:flex items-center justify-center transition-[margin] group-[.side-menu--collapsed]:xl:ms-2 group-[.side-menu--collapsed.side-menu--on-hover]:xl:ms-0 w-full mx-auto">
+          <div className="flex items-center justify-center w-[42px] rounded-lg h-[42px]">
+            <img src="/logo-192×192.png" alt="لوگو" />
+          </div>
+        </a>
         <button
-          onClick={handleClick}
-          className="bg-white p-2 rounded shadow-md hover:bg-gray-100 cursor-pointer"
-          title="موقعیت فعلی"
-        >
-          <Locate className="w-5 h-5 text-gray-700" />
+          onClick={toggleCompactMenu}
+          className="group-[.side-menu--collapsed.side-menu--on-hover]:xl:opacity-100 group-[.side-menu--collapsed]:xl:rotate-180 group-[.side-menu--collapsed]:xl:opacity-0 transition-[opacity,transform] hidden 3xl:flex items-center justify-center w-[20px] h-[20px] ms-auto border rounded-full border-slate-600/40 hover:bg-slate-600/5">
+          <Lucide icon="ArrowLeft" className="w-3.5 h-3.5 stroke-[1.3]" />
         </button>
+        <div className="flex items-center gap-1 xl:hidden">
+          <a
+            href=""
+            onClick={(event) => {
+              event.preventDefault();
+              setActiveMobileMenu(true);
+            }}
+            className="p-2 rounded-full hover:bg-slate-100">
+            <AlignJustify className="w-[18px] h-[18px]" />
+          </a>
+          <a
+            href=""
+            className="p-2 rounded-full hover:bg-slate-100"
+            onClick={(e) => {
+              e.preventDefault();
+              setQuickSearch(true);
+            }}>
+            <Lucide icon="Search" className="w-[18px] h-[18px]" />
+          </a>
+        </div>
       </div>
+      <div className="absolute transition-[padding] duration-100 xl:ps-[275px] group-[.side-menu--collapsed]:xl:ps-[91px] h-full inset-x-0">
+        <div className="flex items-center w-full h-full px-1 md:px-5">
+          {/* BEGIN: Breadcrumb */}
+          <div className="hidden xl:block">
+            <DynamicBreadcrumb />
+          </div>
+
+          {/* END: Breadcrumb */}
+          {/* BEGIN: Search */}
+          <div
+            className="relative  flex-1 hidden xl:flex justify-end"
+            onClick={() => setQuickSearch(true)}>
+            <div className="bg-slate-100 border w-[350px] flex items-center py-2 px-3.5 rounded-[0.5rem] text-slate-400 cursor-pointer hover:bg-slate-50 transition-colors">
+              <Lucide icon="Search" className="w-[18px] h-[18px]" />
+              <div className="ms-2.5 me-auto">جستجوی سریع...</div>
+            </div>
+          </div>
+          <QuickSearch
+            quickSearch={quickSearch}
+            setQuickSearch={setQuickSearch}
+          />
+          {/* END: Search */}
+          {/* BEGIN: Notification & User Menu */}
+          <div className="flex items-center flex-1">
+            <div className="flex items-center gap-1 ms-auto">
+              <a
+                href=""
+                className="p-2 rounded-full hover:bg-slate-100"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setActivitiesPanel(true);
+                }}>
+                <Lucide icon="LayoutGrid" className="w-[18px] h-[18px]" />
+              </a>
+              {/* <a href="" className="p-2 rounded-full hover:bg-slate-100">
+                    <Lucide icon="Goal" className="w-[18px] h-[18px]" />
+                  </a> */}
+              <a
+                href=""
+                className="p-2 rounded-full hover:bg-slate-100 hidden lg:inline-block"
+                onClick={(e) => {
+                  e.preventDefault();
+                  requestFullscreen();
+                }}>
+                <Lucide icon="Expand" className="w-[18px] h-[18px]" />
+              </a>
+              <a
+                href=""
+                className="p-2 rounded-full hover:bg-slate-100"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setNotificationsPanel(true);
+                }}>
+                <Lucide icon="Bell" className="w-[18px] h-[18px]" />
+              </a>
+            </div>
+            <Menu>
+              <Menu.Button className="overflow-hidden rounded-full w-[36px] h-[36px] border-[3px] border-slate-200/70 image-fit">
+                {/* <img
+                  alt="تیل وایز - قالب داشبورد مدیریتی"
+                  src={users.fakeUsers()[0].photo}
+                /> */}
+                {data?.avatar ? (
+                  <img
+                    alt="تصویر پروفایل"
+                    src={data.avatar}
+                    className="cursor-pointer transition-transform hover:scale-110 h-6 w-6 object-cover rounded-full"
+                  />
+                ) : (
+                  <Lucide
+                    icon="User"
+                    className="text-white cursor-pointer transition-transform hover:scale-110 h-6 w-6"
+                  />
+                )}
+              </Menu.Button>
+              <Menu.Items className="w-56 mt-1">
+                <Menu.Item
+                  onClick={() => {
+                    navigate("profile");
+                  }}>
+                  <Lucide icon="User" className="w-4 h-4 me-2" />
+                  {userData?.user ? (
+                    <span>
+                      {userData.user.first_name} {userData.user.last_name}
+                    </span>
+                  ) : (
+                    <span>پروفایل</span>
+                  )}
+                </Menu.Item>
+                {/* <Menu.Divider /> */}
+                {/* <Menu.Item
+                  onClick={() => {
+                    navigate("settings?page=connected-services");
+                  }}>
+                  <Lucide icon="Settings" className="w-4 h-4 me-2" />
+                  خدمات متصل
+                </Menu.Item>
+                <Menu.Item
+                  onClick={() => {
+                    navigate("settings?page=email-settings");
+                  }}>
+                  <Lucide icon="Inbox" className="w-4 h-4 me-2" />
+                  تنظیمات ایمیل
+                </Menu.Item>
+                <Menu.Item
+                  onClick={() => {
+                    navigate("settings?page=security");
+                  }}>
+                  <Lucide icon="Lock" className="w-4 h-4 me-2" />
+                  بازنشانی رمز عبور
+                </Menu.Item> */}
+                <Menu.Divider />
+                {/* <Menu.Item
+                  onClick={() => {
+                    setSwitchAccount(true);
+                  }}>
+                  <Lucide icon="ToggleLeft" className="w-4 h-4 me-2" />
+                  تغییر حساب
+                </Menu.Item> */}
+                <Menu.Item onClick={() => setLogout(true)}>
+                  <Lucide icon="Power" className="w-4 h-4 me-2" />
+                  خروج
+                </Menu.Item>
+              </Menu.Items>
+            </Menu>
+          </div>
+          <ActivitiesPanel
+            activitiesPanel={activitiesPanel}
+            setActivitiesPanel={setActivitiesPanel}
+          />
+          <NotificationsPanel
+            notificationsPanel={notificationsPanel}
+            setNotificationsPanel={setNotificationsPanel}
+          />
+          <SwitchAccount
+            switchAccount={switchAccount}
+            setSwitchAccount={setSwitchAccount}
+          />
+          {/* END: Notification & User Menu */}
+        </div>
+      </div>
+      <Modal
+        open={logout}
+        close={() => setLogout(false)}
+        title="خروج از حساب کاربری"
+        cancelText="انصراف"
+        submitText={isPending ? "درحال خروج" : "خارج میشوم"}
+        onSubmit={() => logoutApi()}
+        variant_cancel="outline-success"
+        variant_submit="outline-danger">
+        <div className="text-center py-2">
+          <span>آیا از خروج از حساب کاربری خود اطمینان دارید؟</span>
+        </div>
+      </Modal>
     </div>
   );
-};
+}
 
-const MapWithLocation = () => {
-  const [position, setPosition] = useState<[number, number] | null>(null);
-
-  return (
-    <MapContainer center={[35.6892, 51.389]} zoom={13} className="h-[400px] w-full">
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-      <LocationPicker onPick={setPosition} />
-
-      {position && (
-        <>
-          <FlyToLocation position={position} />
-          <Marker
-            position={position}
-            draggable
-            eventHandlers={{
-              dragend(e) {
-                const pos = e.target.getLatLng();
-                setPosition([pos.lat, pos.lng]);
-              },
-            }}
-          />
-        </>
-      )}
-
-      <CurrentLocationButton onLocate={setPosition} />
-    </MapContainer>
-  );
-};
-
-export default MapWithLocation;
+export default Topbar;
