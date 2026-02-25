fafbd9a3e4db9cbefab3e73151912cf90868a099
nastaran <nastarantalebi.1382@gmail.com>
2026-02-25 09:25:46 +0330

    handle pointer-events-none when we haven't pagination for accommodation bed and features.

--- src/app/AdminPanel/settings/components/HandlePagination.tsx
+++ src/app/AdminPanel/settings/components/HandlePagination.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@/components/ui/button'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface Props {
   pageCount: number;
@@ -7,15 +7,33 @@ interface Props {
   onPageChange: (page: number) => void;
 }
 
-
-const HandlePagination = ({pageCount, currentPage, onPageChange} : Props) => {
-    
+const HandlePagination = ({ pageCount, currentPage, onPageChange }: Props) => {
   return (
-    <div className='flex flex-row items-end justify-end ml-5 gap-0.5'>
-      <Button size={'icon-sm'} className='rounded-full bg-primary/20 hover:bg-primary/40' onClick={() => currentPage > 1 && onPageChange(currentPage-1)}><ChevronRight/></Button>
-      <Button size={'icon-sm'} className='rounded-full bg-primary/20 hover:bg-primary/40' onClick={() => currentPage < pageCount && onPageChange(currentPage + 1)}><ChevronLeft/></Button>
+    <div className="flex flex-row items-end justify-end ml-5 gap-0.5 ">
+      <Button
+        size={"icon-sm"}
+        className={
+          currentPage === 1
+            ? "pointer-events-none opacity-50 rounded-full bg-primary/20 hover:bg-primary/40"
+            : "rounded-full bg-primary/20 hover:bg-primary/40"
+        }
+        onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+      >
+        <ChevronRight />
+      </Button>
+      <Button
+        size={"icon-sm"}
+        className={
+          currentPage === pageCount
+            ? "pointer-events-none opacity-50 rounded-full bg-primary/20 hover:bg-primary/40"
+            : "rounded-full bg-primary/20 hover:bg-primary/40"
+        }
+        onClick={() => currentPage < pageCount && onPageChange(currentPage + 1)}
+      >
+        <ChevronLeft />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default HandlePagination
+export default HandlePagination;
