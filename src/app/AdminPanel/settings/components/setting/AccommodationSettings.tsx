512ed7db8663f18af3f18eaf226929cee806e7c2
nastaran <nastarantalebi.1382@gmail.com>
2026-03-26 17:21:59 +0330

    make setting responsive

--- src/app/AdminPanel/settings/components/setting/AccommodationSettings.tsx
+++ src/app/AdminPanel/settings/components/setting/AccommodationSettings.tsx
@@ -3,13 +3,13 @@ import { settingTabs } from "../../fixtures/settingTabsData";
 
 const AccommodationSettings = () => {
   return (
-    <div className="py-8">
+    <div className="">
       <Tabs
         defaultValue={settingTabs[0].title}
         orientation="vertical"
-        className="flex flex-row items-start gap-6"
+        className="flex md:flex-row flex-col items-start gap-10"
       >
-        <TabsList className="flex flex-col h-auto w-48 shrink-0 sticky top-4 py-3 px-3 gap-1 ">
+        <TabsList className="h-auto w-48 shrink-0 py-3 px-3 gap-1 ">
           {settingTabs.map((item) => (
             <TabsTrigger
               key={item.title}
