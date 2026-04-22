9f65f0e8975f45e95f1389db2dc3e56c71c0806b
nastaran <nastarantalebi.1382@gmail.com>
2026-04-22 14:14:09 +0330

    feat(add admin buuton in homePage)[deploy}

--- src/app/usersPanel/components/HeroSection.tsx
+++ src/app/usersPanel/components/HeroSection.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import SearchForm from "./SearchForm";
+import CustomButton from "@/components/form/CustomButton";
+import { ArrowUpRight } from "lucide-react";
+import { useNavigate } from "@tanstack/react-router";
 
 const HeroSection = () => {
   const [animate, setAnimate] = useState(false);
+  const navigate = useNavigate()
 
   useEffect(() => {
     // یک تیک تأخیر برای اینکه CSS transition درست کار کند
@@ -19,6 +23,15 @@ const HeroSection = () => {
         backgroundImage: "url('./accommodationHero.png')",
       }}
     >
+      <div className="absolute top-5 right-5 flex items-center gap-2">
+        <CustomButton onClick={() => navigate({to: "/login"})} className="bg-white text-black font-semibold shadow-md hover:bg-gray-100">
+          <div className="flex flex-row items-center gap-2">
+            <ArrowUpRight className="w-4 h-4" />
+            <span>پنل ادمین</span>
+          </div>
+        </CustomButton>
+      </div>
+
       <div className="flex flex-col justify-center items-center gap-5">
         <h1 className="text-white text-4xl font-bold">مقصدتان کجاست؟</h1>
         <h3 className="text-white text-xl font-bold">
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,18 +1,21 @@
 import Login from "@/app/login/components/Login";
+import useRefresh from "@/app/login/services/useRefresh";
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  // const { mutateAsync: refresh, isPending } = useRefresh();
+  
+  const { mutateAsync: refresh, isPending } = useRefresh();
 
-  // useEffect(() => {
-  //   refresh();
-  // }, []);
+  useEffect(() => {
+    refresh();
+  }, []);
 
-  // if (isPending) return <div>Loading...</div>;
+  if (isPending) return <div>Loading...</div>;
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <Login />
