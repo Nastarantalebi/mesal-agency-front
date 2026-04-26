8b3aca37fb497c888895f6542c1b80d338370062
nastaran <nastarantalebi.1382@gmail.com>
2026-04-26 15:25:30 +0330

    feat(handle pagination in acommodations features and implement one card in features components)

--- src/app/AdminPanel/Accommodation/components/AccommodationFeatures.tsx
+++ src/app/AdminPanel/Accommodation/components/AccommodationFeatures.tsx
@@ -7,17 +7,28 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { X } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { accommodationFeatureListInitialValues, accommodationFeatureListValidation } from "../fixtures/Validation";
+import {
+  accommodationFeatureListInitialValues,
+  accommodationFeatureListValidation,
+} from "../fixtures/Validation";
 import { useAccommodationFeatures } from "../services/useAccommodation";
-import type {
-  Props,
-  TCAccommodationFeature,
-  TFeatureListForm
-} from "../types";
+import type { Props, TCAccommodationFeature, TFeatureListForm } from "../types";
+import CardPagination from "@/components/card/CardPagination";
 
 const AccommodationFeatures = ({ AccommodationId }: Props) => {
+  const {
+    accommodationFeatures,
+    accommodationFeatureList,
+    postAccommodationFeatures,
+    deleteAccommodatioFeature,
+  } = useAccommodationFeatures(AccommodationId);
 
-  const { accommodationFeatures, accommodationFeatureList, postAccommodationFeatures, deleteAccommodatioFeature} = useAccommodationFeatures(AccommodationId);
+  const [currentAccommodationFeaturePage, setCurrentAccommodationFeaturePage] =
+    useState(1);
+
+  const accommodationFeaturesPageCount = accommodationFeatures.data?.count
+    ? Math.ceil(accommodationFeatures.data.count / 10)
+    : 0;
 
   const form = useForm<TFeatureListForm>({
     resolver: zodResolver(accommodationFeatureListValidation),
@@ -26,30 +37,27 @@ const AccommodationFeatures = ({ AccommodationId }: Props) => {
 
   const [errorOpen, setErrorOpen] = useState(false);
   const errmessage = "ثبت فرم با خطا مواجه شد، لطفاً دوباره تلاش کنید.";
-  
 
   const [selectedIds, setSelectedIds] = useState<number[]>([]);
-  
+
   const allAddedFeaturesIds = accommodationFeatureList.data?.results.map(
     (r) => r.feature.id,
   );
 
-  
   const toggle = (id: number) => {
-
     setSelectedIds((prev) => {
       const newSelection = prev.includes(id)
         ? prev.filter((x) => x !== id)
         : [...prev, id];
 
-        const resultSelection = Array.from(new Set([...newSelection, ...(allAddedFeaturesIds || [])]));
+      const resultSelection = Array.from(
+        new Set([...newSelection, ...(allAddedFeaturesIds || [])]),
+      );
 
-        form.setValue("feature", resultSelection, { shouldValidate: true });
-        return resultSelection;
-      });
-    };
-
-    
+      form.setValue("feature", resultSelection, { shouldValidate: true });
+      return resultSelection;
+    });
+  };
 
   const handleSubmit = (values: TCAccommodationFeature) => {
     postAccommodationFeatures.mutateAsync(
@@ -63,75 +71,61 @@ const AccommodationFeatures = ({ AccommodationId }: Props) => {
     );
   };
 
-
-
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <div className="grid grid-cols-2">
-          <Card className="grid-cols-1 ml-2">
-            <CardTitle className="mx-5"> ویژگی های مربوط به اقامتگاه</CardTitle>
-            {accommodationFeatures.data ? (
-              <CardContent>
-                <div className="flex flex-wrap gap-2">
-                  {accommodationFeatures.data.results?.map((f) => {
-                    const isAdded = allAddedFeaturesIds?.includes(f.id);
-                    const selected = selectedIds.includes(f.id) && !isAdded;
-                    return (
-                      <Badge
-                        key={f.id}
-                        variant="outline"
-                        onClick={() => {!isAdded && toggle(f.id)}}
-                        className={`
-                          cursor-pointer px-6 py-2
-                          ${isAdded ? "cursor-not-allowed bg-accent/20 border-accent" : ""}
+        <Card className="w-fit mt-10">
+          <CardTitle className="mx-5"> ویژگی های مربوط به اقامتگاه</CardTitle>
+          {accommodationFeatures.data ? (
+            <CardContent>
+              <div className="flex flex-wrap gap-2">
+                {accommodationFeatures.data.results?.map((f) => {
+                  const isAdded = allAddedFeaturesIds?.includes(f.id);
+                  const selected = selectedIds.includes(f.id) && !isAdded;
+                  return (
+                    <Badge
+                      key={f.id}
+                      variant="outline"
+                      onClick={() => {
+                        !isAdded && toggle(f.id);
+                      }}
+                      className={`
+                          cursor-pointer pl-5 py-2 relative 
+                          ${isAdded ? "bg-accent/20 border-accent pr-10" : ""}
                           ${selected ? "bg-green-400/10 text-black border-green-400" : ""}
                         `}
-                      >
-                        {f.title}
-                      </Badge>
-                    );
-                  })}
-                </div>
-              </CardContent>
-            ) : (
-              <CardContent>داده ای برای نمایش وجود ندارد</CardContent>
-            )}
-          </Card>
-          <Card>
-            <CardTitle className="mx-5">ویژگی های افزوده شده</CardTitle>
-            {accommodationFeatureList.data?.results.length ? (
-              <CardContent className="">
-                <div className="flex flex-wrap gap-2">
-                  {accommodationFeatureList.data?.results.map((f) => {
-                    return (
-                      <>
-                        <Badge
-                          key={f.id}
-                          variant="primary"
-                          className="px-6 py-2 bg-accent/70 text-black relative pr-10"
-                        >
-                          {f.feature.title}
-                          <button
-                            type="button"
-                            onClick={() => {deleteAccommodatioFeature.mutateAsync({ id: f.id }); setSelectedIds(selectedIds.filter((id) => id !== f.id));}}
-                            className="absolute right-1 top-1/2 -translate-y-1/2 bg-destructive/20 hover:bg-destructive/40 rounded-full p-1.5 cursor-pointer"
-                          >
-                            <X className="h-3 w-3" />
-                          </button>
-                        </Badge>
-                      </>
-                    );
-                  })}
-                </div>
-              </CardContent>
-            ) : (
-              <CardContent>
-                هیچ ویژگی اضافه نشده است. از لیست سمت راست اضافه کنید.
-              </CardContent>
-            )}
-          </Card>
-        </div>
+                    >
+                      {accommodationFeatureList.data?.results.map(
+                        (feature) =>
+                          feature.feature.id === f.id && (
+                            <button
+                              className="absolute right-1 top-1/2 -translate-y-1/2 bg-destructive/20 hover:bg-destructive/40 rounded-full p-1.5 cursor-pointer"
+                              onClick={() =>
+                                deleteAccommodatioFeature.mutateAsync({
+                                  id: feature.id,
+                                })
+                              }
+                            >
+                              <X className="h-3 w-3" />
+                            </button>
+                          ),
+                      )}
+                      {f.title}
+                    </Badge>
+                  );
+                })}
+              </div>
+            </CardContent>
+          ) : (
+            <CardContent>داده ای برای نمایش وجود ندارد</CardContent>
+          )}
+          <CardPagination
+            currentPage={currentAccommodationFeaturePage}
+            onPageChange={setCurrentAccommodationFeaturePage}
+            pageCount={accommodationFeaturesPageCount}
+          />
+        </Card>
+
         <CustomButton type="submit" className="mt-5">
           ثبت
         </CustomButton>
--- src/app/AdminPanel/RoomTypes/components/roomType/RoomTypeFeatures.tsx
+++ src/app/AdminPanel/RoomTypes/components/roomType/RoomTypeFeatures.tsx
@@ -96,87 +96,58 @@ const RoomTypeFeatures = ({
     <div className="sm:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-4xl ">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(handleSubmit)}>
-          <div className="grid grid-cols-2 gap-10">
-            <div className="flex flex-col gap-5 items-start justify-start">
-              <Card className="shadow-lg shadow-primary/50">
-                <CardTitle className="text-center text-sm font-light mr-5">
-                  ویژگی های مربوط به اتاق
-                </CardTitle>
-                {getFeatures.data ? (
-                  <CardContent>
-                    <div className="flex flex-wrap gap-2">
-                      {getFeatures.data.results?.map((f) => {
-                        const isAdded = allAddedFeaturesIds?.includes(f.id);
-                        const selected = selectedIds.includes(f.id) && !isAdded;
-                        return (
-                          <Badge
-                            key={f.id}
-                            variant="outline"
-                            onClick={() => {
-                              !isAdded && toggle(f.id);
-                            }}
-                            className={`cursor-pointer px-6 py-2 ${isAdded ? "cursor-not-allowed bg-accent/20 border-accent" : ""} ${selected ? "bg-green-400/10 text-black border-green-400 cursor-pointer" : ""}`}
-                          >
-                            {f.title}
-                          </Badge>
-                        );
-                      })}
-                    </div>
-                  </CardContent>
-                ) : (
-                  <CardContent>داده ای برای نمایش وجود ندارد</CardContent>
-                )}
-                <CardPagination
-                  currentPage={currentRoomTypeFeaturePage}
-                  onPageChange={setCurrentRoomTypeFeaturePage}
-                  pageCount={roomTypeFeaturesPageCount}
-                />
-              </Card>
-              <CustomButton type="submit">ثبت</CustomButton>
-            </div>
-            <div className="flex flex-col gap-5 items-start justify-start">
-              <Card className="shadow-lg shadow-primary/50">
-                <CardTitle className="text-center text-sm font-light mx-5">
-                  ویژگی های اضافه شده
-                </CardTitle>
-                {getRoomTypeFeatures.data?.length ? (
-                  <CardContent className="p-5">
-                    <div className="flex flex-wrap gap-2">
-                      {getRoomTypeFeatures.data?.map((f) => {
-                        return (
-                          <Badge
-                            key={f.id}
-                            variant="primary"
-                            className="px-6 py-2 bg-accent/70 text-black relative pr-10"
-                          >
-                            {f.feature.title}
-                            <button
-                              type="button"
-                              onClick={() => {
-                                deleteRoomTypeFeatures.mutateAsync({
-                                  id: f.id,
-                                });
-                                setSelectedIds(
-                                  selectedIds.filter((id) => id !== f.id),
-                                );
-                              }}
-                              className="absolute right-1 top-1/2 -translate-y-1/2 bg-destructive/20 hover:bg-destructive/40 rounded-full p-1.5 cursor-pointer"
-                            >
-                              <X className="h-3 w-3" />
-                            </button>
-                          </Badge>
-                        );
-                      })}
-                    </div>
-                  </CardContent>
-                ) : (
-                  <CardContent className="p-5">
-                    هیچ ویژگی اضافه نشده است. از لیست سمت راست اضافه کنید.
-                  </CardContent>
-                )}
-              </Card>
-            </div>
-          </div>
+          <Card className="shadow-lg shadow-primary/50 w-fit">
+            <CardTitle className="text-center text-sm font-light mr-5">
+              ویژگی های مربوط به اتاق
+            </CardTitle>
+            {getFeatures.data ? (
+              <CardContent>
+                <div className="flex flex-wrap gap-2">
+                  {getFeatures.data.results?.map((f) => {
+                    const isAdded = allAddedFeaturesIds?.includes(f.id);
+                    const selected = selectedIds.includes(f.id) && !isAdded;
+                    return (
+                      <Badge
+                        key={f.id}
+                        variant="outline"
+                        onClick={() => {
+                          !isAdded && toggle(f.id);
+                        }}
+                        className={`cursor-pointer px-6 py-2 relative ${isAdded ? "cursor-not-allowed bg-accent/20 border-accent pr-10" : ""} ${selected ? "bg-green-400/10 text-black border-green-400 cursor-pointer" : ""}`}
+                      >
+                        {getRoomTypeFeatures?.data?.map(
+                          (feature) =>
+                            feature.feature.id === f.id && (
+                              <button
+                                className="absolute right-1 top-1/2 -translate-y-1/2 bg-destructive/20 hover:bg-destructive/40 rounded-full p-1.5 cursor-pointer"
+                                onClick={() =>
+                                  deleteRoomTypeFeatures.mutateAsync({
+                                    id: feature.id,
+                                  })
+                                }
+                              >
+                                <X className="h-3 w-3" />
+                              </button>
+                            ),
+                        )}
+                        {f.title}
+                      </Badge>
+                    );
+                  })}
+                </div>
+              </CardContent>
+            ) : (
+              <CardContent>داده ای برای نمایش وجود ندارد</CardContent>
+            )}
+            <CardPagination
+              currentPage={currentRoomTypeFeaturePage}
+              onPageChange={setCurrentRoomTypeFeaturePage}
+              pageCount={roomTypeFeaturesPageCount}
+            />
+          </Card>
+          <CustomButton type="submit" className="mt-5">ثبت</CustomButton>
+
+          <div className="flex flex-col gap-5 items-start justify-start"></div>
         </form>
         <FormErrorModal
           open={errorOpen}
